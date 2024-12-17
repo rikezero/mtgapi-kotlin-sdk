5ce61b31c61fe23b91f07ffeab2c550f26d9e1ac
Rike <rikezero@gmail.com>
2024-12-17 02:41:59 -0300

    feat: Update README and updated modules file #publish

--- README.md
+++ README.md
@@ -1,8 +1,82 @@
 # Kotlin SDK for using the magicthegathering.io APIs
 
-I've made this one since the other Kotlin SDK seems to not be maintained anymore and I decided to try and make a new one, adding UseCases and better suiting it to my tastes. Feel free to use or fork it.
+## Overview
 
-WIP - I'll be updating this in order to complete the development and finish all the setups and little details here and there.
+This Kotlin SDK is built to interact with the [Magic: The Gathering API](https://magicthegathering.io), providing a clean,
+Kotlin-centric interface to access the API’s data. The SDK is designed to offer a more modern and structured approach with
+features such as UseCases, tailored for better flexibility and ease of use compared to other SDKs available.
+
+The SDK was developed to offer a more modern alternative to the officially listed Kotlin SDK for the Magic: The Gathering API,
+which also don't appear to be maintained anymore.
+It aims to provide developers with a better, more organized way to interact with the API while being easy to integrate into
+your Kotlin-based applications.
+
+The [Magic: The Gathering API documentation](https://docs.magicthegathering.io/#documentationgetting_started) can be found here
+for a deeper understanding of how the API works.
+
+Feel free to use or fork it as per your needs!
+
+**WIP** - Ongoing updates to complete development and polish features.
 
 ## Version
-Current SDK version: v1.0.0.23
+Current SDK version: **v1.0.0.23**
+
+---
+
+## Installation
+
+There are two methods you can choose from to install the MTG API Kotlin SDK into your project: via GitHub Packages (recommended) or by manually downloading the `.jar` file.
+
+### Option 1: GitHub Packages (Recommended)
+
+You can use GitHub Packages to easily install the SDK into your project using Gradle. Follow the instructions below:
+
+1. Add the GitHub Packages repository to your `build.gradle` file:
+
+    ```gradle
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/rikezero/mtgapi-kotlin-sdk")
+            credentials {
+                username = project.findProperty("gpr.user") ?: System.getenv("USERNAME_GITHUB")
+                password = project.findProperty("gpr.token") ?: System.getenv("TOKEN_GITHUB")
+            }
+        }
+    }
+    ```
+
+2. Add the dependency for the SDK:
+
+    ```gradle
+    dependencies {
+        implementation 'com.rikezero:mtgapi-kotlin-sdk:v1.0.0.23'
+    }
+    ```
+
+   Make sure to replace `v1.0.0.23` with the appropriate version number as needed.
+
+3. To authenticate to GitHub Packages, create a personal access token on GitHub with the appropriate permissions and set the `gpr.user` and `gpr.token` properties in your project (or environment variables).
+
+For more information about working with GitHub Packages, check out the official [GitHub Packages documentation](https://docs.github.com/pt/packages/working-with-a-github-packages-registry/working-with-the-gradle-registry).
+
+### Option 2: Manual Installation
+
+If you prefer not to use GitHub Packages, you can manually download the `.jar` file from the releases page of this repository.
+
+1. Visit the [Releases page](https://github.com/rikezero/mtgapi-kotlin-sdk/releases) on GitHub.
+2. Download the `.jar` file corresponding to the release you want to use.
+3. Add the `.jar` file to your project's `libs` directory (or any folder where you store external dependencies).
+4. In your `build.gradle` file, add the following to include the `.jar` file in your project:
+
+    ```gradle
+    dependencies {
+        implementation files('libs/mtgapi-kotlin-sdk-v1.0.0.23.jar')
+    }
+    ```
+
+Make sure to replace `mtgapi-kotlin-sdk-v1.0.0.23.jar` with the actual filename of the `.jar` file you downloaded.
+
+---
+
+
+
--- src/main/java/com/rikezero/mtgapi_kotlin_sdk/di/MtgApiModule.kt
+++ src/main/java/com/rikezero/mtgapi_kotlin_sdk/di/MtgApiModule.kt
@@ -1,6 +1,16 @@
 package com.rikezero.mtgapi_kotlin_sdk.di
 
 import com.rikezero.mtgapi_kotlin_sdk.config.BuildConfig
+import com.rikezero.mtgapi_kotlin_sdk.domain.repository.MtgApiRepository
+import com.rikezero.mtgapi_kotlin_sdk.domain.repository.impl.MtgApiRepositoryImpl
+import com.rikezero.mtgapi_kotlin_sdk.domain.usecase.GetCardByIdUseCase
+import com.rikezero.mtgapi_kotlin_sdk.domain.usecase.GetCardsUseCase
+import com.rikezero.mtgapi_kotlin_sdk.domain.usecase.GetFormatsUseCase
+import com.rikezero.mtgapi_kotlin_sdk.domain.usecase.GetSetByIdUseCase
+import com.rikezero.mtgapi_kotlin_sdk.domain.usecase.GetSetsUseCase
+import com.rikezero.mtgapi_kotlin_sdk.domain.usecase.GetSubTypesUseCase
+import com.rikezero.mtgapi_kotlin_sdk.domain.usecase.GetSuperTypesUseCase
+import com.rikezero.mtgapi_kotlin_sdk.domain.usecase.GetTypesUseCase
 import com.rikezero.mtgapi_kotlin_sdk.networking.MtgApiNetworking
 import com.rikezero.mtgapi_kotlin_sdk.networking.engine.MtgApiNetworkEngine
 import com.rikezero.mtgapi_kotlin_sdk.networking.engine.impl.MtgApiNetworkEngineImpl
@@ -8,12 +18,13 @@ import com.rikezero.mtgapi_kotlin_sdk.networking.engine.retrofit.buildMtgApiRetr
 import com.rikezero.mtgapi_kotlin_sdk.networking.impl.MtgApiNetworkingImpl
 import com.rikezero.mtgapi_kotlin_sdk.networking.networkadapter.MtgApiNetworkAdapter
 import com.rikezero.mtgapi_kotlin_sdk.networking.networkadapter.impl.MtgApiNetworkAdapterImpl
+import org.koin.core.context.loadKoinModules
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 const val NAME_RETROFIT_MTG = "retrofit_mtgapi"
 
-val mtgApiNetworkingModules = module {
+private val mtgApiNetworkingModules = module {
     single(named(NAME_RETROFIT_MTG)) {
         buildMtgApiRetrofit(
             host = BuildConfig.MAGIC_THE_GATHERING_BASE_URL,
@@ -23,4 +34,58 @@ val mtgApiNetworkingModules = module {
     single<MtgApiNetworkEngine> { MtgApiNetworkEngineImpl(retrofit = get()) }
     single<MtgApiNetworkAdapter> { MtgApiNetworkAdapterImpl(networkEngine = get()) }
     single<MtgApiNetworking> { MtgApiNetworkingImpl(networkAdapter = get()) }
+}
+
+private val mtgApiRepositoryModules = module {
+    single<MtgApiRepository> { MtgApiRepositoryImpl(mtgApiNetworking = get()) }
+}
+
+private val mtgApiUseCaseModules = module {
+    single<GetCardsUseCase> { GetCardsUseCase(mtgApiRepository = get()) }
+    single<GetCardByIdUseCase> { GetCardByIdUseCase(mtgApiRepository = get()) }
+    single<GetSetsUseCase> { GetSetsUseCase(mtgApiRepository = get()) }
+    single<GetSetByIdUseCase> { GetSetByIdUseCase(mtgApiRepository = get()) }
+    single<GetFormatsUseCase> { GetFormatsUseCase(mtgApiRepository = get()) }
+    single<GetTypesUseCase> { GetTypesUseCase(mtgApiRepository = get()) }
+    single<GetSubTypesUseCase> { GetSubTypesUseCase(mtgApiRepository = get()) }
+    single<GetSuperTypesUseCase> { GetSuperTypesUseCase(mtgApiRepository = get()) }
+}
+
+private val mtgApiLibraryModules = listOf(
+    mtgApiUseCaseModules,
+    mtgApiRepositoryModules,
+    mtgApiNetworkingModules
+)
+
+/**
+ * Initializes the MTG API library by loading the necessary Koin modules.
+ *
+ * This function is responsible for setting up the dependencies required for the MTG API library.
+ * It should be called **before** using any features or services provided by the library to ensure
+ * that the necessary modules are loaded into the Koin dependency injection container.
+ *
+ * This function should be called after Koin has been initialized in the application (e.g., after
+ * calling `startKoin()`), and it will load the modules specific to the MTG API library.
+ *
+ * Calling this function multiple times has no effect; the modules will only be loaded once.
+ *
+ * Example usage:
+ * ```
+ * // In your Application class, Koin is initialized first:
+ * class MyApplication : Application() {
+ *     override fun onCreate() {
+ *         super.onCreate() *
+ *         // Initialize Koin when the app starts
+ *         startKoin {
+ *             androidContext(this@MyApplication)
+ *             modules(initialModules)  // This could be your global/default modules
+ *         } *
+ *         // Now, load the MTG API library modules dynamically
+ *         startMtgApiLibrary()
+ *     }
+ * }
+ * ```
+ */
+fun startMtgApiLibrary() {
+    loadKoinModules(mtgApiLibraryModules)
 }
